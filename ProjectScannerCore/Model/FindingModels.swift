24d38e118ea347d485cc2c230d6f44179f7ea915
Luke <hello@talktoluke.com>
2026-08-23 11:51:27 +0100

    Define project scanner models and limits

---
+++ ProjectScannerCore/Model/FindingModels.swift
@@ -0,0 +1,36 @@
+public struct SessionFindingHeader: Sendable, Equatable {
+    public let kind: FindingKind
+    public let ruleID: RuleID
+    public let ruleVersion: UInt32
+    public let sourceView: SourceView
+    public let detectorID: DetectorID
+    public let coverageTransactionID: CoverageTransactionID
+    public let assessment: FindingAssessment
+    public let provenance: FindingProvenance
+    public let suppressionEligibility: SuppressionEligibility
+    public let suppressionState: SessionSuppressionState
+
+    public init(
+        kind: FindingKind,
+        ruleID: RuleID,
+        ruleVersion: UInt32,
+        sourceView: SourceView,
+        detectorID: DetectorID,
+        coverageTransactionID: CoverageTransactionID,
+        assessment: FindingAssessment,
+        provenance: FindingProvenance,
+        suppressionEligibility: SuppressionEligibility,
+        suppressionState: SessionSuppressionState
+    ) {
+        self.kind = kind
+        self.ruleID = ruleID
+        self.ruleVersion = ruleVersion
+        self.sourceView = sourceView
+        self.detectorID = detectorID
+        self.coverageTransactionID = coverageTransactionID
+        self.assessment = assessment
+        self.provenance = provenance
+        self.suppressionEligibility = suppressionEligibility
+        self.suppressionState = suppressionState
+    }
+}
